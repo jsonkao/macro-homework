68bd4ee71868f10d5fb116f0075797ef69f897dd
Jason Kao <jason.kao@columbia.edu>
2020-01-28 13:21:22 -0500

    check over/debug

--- hw1.py
+++ hw1.py
@@ -6,14 +6,16 @@ data = {
 years = [1870, 1913, 1929]
 for c in data.keys():
     data[c] = { years[i]: value for i, value in enumerate(data[c]) }
+longnames = {
+    'IR': 'Ireland',
+    'US': 'the U.S.',
+    'AR': 'Argentina',
+}
 
 def percent_diff(c1, c2, year):
     answer = data[c1][year] / data[c2][year]
     print(f'{answer:.3f}; {(answer - 1) * 100:.1f}%')
     return answer
-
-percent_diff('IR', 'US', 1870)
-percent_diff('IR', 'AR', 1870)
 
 def avg_growth(c, year1, year2):
     return (data[c][year2] / data[c][year1]) ** (1 / (year2 - year1))
@@ -30,8 +32,10 @@ def pv(c, y1, y2):
     return output
 
 for key in data:
-    # print(f'{key}: {(avg_growth(key, 1870, 1913) - 1) * 100:.2f}%')
-    # print(f'{key}: {(forecast(key, 1870, 1913, 1929)):.0f}')
-    print(f'{key}: {(pv(key, 1870, 1913)):,.0f}')
+    # print(f'that of {longnames[key]} was {(avg_growth(key, 1870, 1913) - 1) * 100:.2f}\\%;', end = ' ')
+    # print(f'that for {longnames[key]} was {(pv(key, 1870, 1913)):,.0f};', end = ' ')
+    # print(f'{(forecast(key, 1870, 1913, 1929)):,.0f} in {longnames[key]};', end = ' ')
+    # print(f'that for {longnames[key]} was {(pv(key, 1913, 1929)):,.0f};', end = ' ')
+    print(f'that for {longnames[key]} was {pv(key, 1913, 1929) + pv(key, 1870, 1913):,.0f};', end = ' ')
 
 
